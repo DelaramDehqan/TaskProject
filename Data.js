9fdbebacc7cc019a8e59b71f6a3eccc2ab47efda
DelaramDehqan <103545728+DelaramDehqan@users.noreply.github.com>
2022-09-06 02:10:47 +0430

    pushing first time project repository

---
+++ Data.js
@@ -0,0 +1,18 @@
+export default [
+  [
+    {
+      title: "first card",
+      name: "Kayaking",
+      price: 39 / 4,
+      locatin: "Derina,Serbia"
+    }
+  ],
+  [
+    {
+      title: "second card",
+      name: "Camping",
+      price: 29 / 8,
+      location: "Tara,Serbia"
+    }
+  ]
+];
